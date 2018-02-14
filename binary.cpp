8217de22022f6b5f6db9f7e6728fc4897ac93523
Shadow1246 <Shadow1246@users.noreply.github.com>
2018-02-14 11:33:45 -0400

    Added binary file

---
+++ binary.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main( )
+{
+   //create some data
+   short int a = -6730;
+   float b = 68.123; 
+   char c = 'J';
+   ofstream abc{"abc.bin"};
+
+	if (abc)
+	{
+		abc.write(reinterpret_cast<char>(&a)sizeof(a); 
+		abc.write(reinterpret_cast<char>(&b)sizeof(b); 
+		abc.write(reinterpret_cast<char>(&c)sizeof(c); 
+	}
+	else
+	{
+		cout << "Error writing to file ";
+	}
+ 
+   cout<<a<<" "<<b<<" "<<c;//display data to console (i.e. as formatted chars)
+
+   cout<<endl<<endl;
+ 
+   return 0;
+}
